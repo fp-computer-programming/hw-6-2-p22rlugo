d49e91ec73da7da2aabe02b9ee114b35341263a6
p22rlugo <p22rlugo@fairfieldprep.org>
2021-11-15 09:47:30 -0500

    Add file

--- hw6-2.py
+++ hw6-2.py
@@ -8,3 +8,25 @@ print(len(lst_one))
 
 # Question 3
 print(lst_one[2])
+
+# Question 4
+print(5 not in lst_one)
+
+# Question 5
+print(10 not in lst_one)
+
+# Question 6
+another_list = ["Jack","Luis","Ryan"]
+another_list = another_list + ["Yes"]
+
+# Question 7
+print(another_list[2])
+
+# Question 8
+another_list * 3
+
+# Question 9
+print(lst_one + another_list)
+
+# Quesiton 10
+print(len(lst_one + another_list))
